2144caa21ef3620a5faf0b266bf27b5b0729980c
DerekNoguera <dereknoguera9@gmail.com>
2024-04-29 12:46:42 -0600

    puse validaciones en los campos

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ let boton1 = document.getElementById("botonschool")
 let div1 = document.getElementById("div1f")
 
 boton1.addEventListener("click", function () {
-    let school = document.createElement("li")
-    school.innerHTML = inputxt1.value;
-    div1.appendChild(school)
+    if (inputxt1.value === "") {
+        alert("No puedes dejar el campo en blanco");
+    } else {
+        let school = document.createElement("li")
+        school.innerHTML = inputxt1.value;
+        div1.appendChild(school)
+    }
 })
 
 let inputxt2 = document.getElementById("textschool2")
@@ -13,8 +17,12 @@ let boton2 = document.getElementById("botonschool2")
 let div2 = document.getElementById("div2f")
 
 boton2.addEventListener("click", function () {
-    let school2 = document.createElement("li")
-    school2.innerHTML = inputxt2.value;
-    div2.appendChild(school2)
+    if (inputxt2.value === "") {
+        alert("No puedes dejar el campo en blanco")
+    } else {
+        let school2 = document.createElement("li")
+        school2.innerHTML = inputxt2.value;
+        div2.appendChild(school2)
+    }
 })
 
